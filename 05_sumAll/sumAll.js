1d20ff36f669fa2b1533a5c2ebe42480c06ea6d5
diogocarrola <diogo.carrola@gmail.com>
2024-06-18 22:23:56 +0100

    Implemented sumAll function with input validation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if (x < 0 || y < 0) return "ERROR";
+    let sum = 0;
+    if(x < y) {
+        while(x <= y) {
+            sum += x;
+            x++;
+        }
+        return sum;
+    }
+    if (x > y) {
+        while(x >= y) {
+            sum += x;
+            x--;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
